6449cb182a8e39023c3a28caedd12edd46b50a08
kisore <kisore@DESKTOP-L5V70GO>
2025-04-11 10:30:50 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## food
+
+task
+
+#### License
+
+MIT
---
+++ food_order_app/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "food",
+			"type": "module",
+			"label": _("food")
+		}
+	]
