243f877fc5c8af5595bca6908760bfb252f5e4a9
kisore2003 <kisores37@gmail.com>
2025-04-14 12:35:49 +0530

    fixed

--- food_order_app/food/doctype/cooking_court/cooking_court.js
+++ food_order_app/food/doctype/cooking_court/cooking_court.js
@@ -1,6 +1,19 @@
 // Copyright (c) 2025, kisore and contributors
 // For license information, please see license.txt
 
+frappe.ui.form.on('Cooking Court', {
+    status: function(frm) {
+        const status = frm.doc.status;
+
+        frm.doc.confirm_order.forEach(function(row) {
+            frappe.model.set_value(row.doctype, row.name, 'status', status);
+        });
+
+        frm.fields_dict.confirm_order.grid.update_docfield_property('status', 'read_only', 1);
+
+        frm.refresh_field('confirm_order');
+    }
+});
 
 
 
